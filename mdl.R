7268a63a48ea015b163f800547343e299c531f0d
FroggyZ <Yvette.Bonvalot@GMail.com>
2014-11-27 11:13:42 -0500

    Some minor changes to mdl.R file.

--- mdl.R
+++ mdl.R
@@ -1,7 +1,10 @@
+install.packages("psych")
+install.packages("MASS")
+
 library(psych)
 library(MASS)
 
-setwd("/Users/claireaustin/Desktop/")
+setwd("C:/RWork/NAPS")
 getwd()
 
 # mdl <- read.csv (file="mdl_urban_nonpolar.csv",head=TRUE,sep=",")
@@ -15,12 +18,16 @@ str(mdl)
 
 # Basic plots
 plot(mdl$date, mdl$XBenzene)
-hist(mdl$XBenzene,main="Distribution of Benzene",xlab="ug/m3")
+hist(mdl$XBenzene,
+     main="Distribution od detection limit for Benzene (2003-11-10 - 2014-04-01)",
+     xlab="µg/m3")
 boxplot(mdl$XBenzene)
 qqnorm(mdl$XBenzene); qqline(mdl$XBenzene)
 
 plot(mdl$date, mdl$XToluene)
-hist(mdl$XToluene,main="Distribution of Toluene",xlab="ug/m3")
+hist(mdl$XToluene,
+     main="Distribution od detection limit for Toluene (2003-11-10 - 2014-04-01)",
+     xlab="µg/m3")
 boxplot(mdl$XToluene)
 qqnorm(mdl$XToluene); qqline(mdl$XToluene)
 
@@ -38,5 +45,3 @@ summary(mdl$XToluene)
 
 # Save dataframe
 # save(mdl,file="mdl_urban_nonpolar.Rda")
-
-
